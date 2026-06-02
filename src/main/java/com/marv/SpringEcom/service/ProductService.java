eae7ffdf924f4d42b478fc2cc2fd521fec08b9fe
MarvsConcept <Marvade8@gmail.com>
2026-06-02 16:37:29 -0700

    Search Feature

--- src/main/java/com/marv/SpringEcom/service/ProductService.java
+++ src/main/java/com/marv/SpringEcom/service/ProductService.java
@@ -46,4 +46,9 @@ public class ProductService {
 
         productRepo.deleteById(id);
     }
+
+    public List<Product> searchProducts(String keyword) {
+
+        return productRepo.searchProducts(keyword);
+    }
 }
