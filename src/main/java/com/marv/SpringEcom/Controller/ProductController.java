172c604754998ccdc4625f89dd561dbec61f75de
MarvsConcept <Marvade8@gmail.com>
2026-06-01 18:06:28 -0700

    Endpoint to get all products

--- src/main/java/com/marv/SpringEcom/Controller/ProductController.java
+++ src/main/java/com/marv/SpringEcom/Controller/ProductController.java
@@ -1,6 +1,9 @@
 package com.marv.SpringEcom.Controller;
 
 import com.marv.SpringEcom.model.Product;
+import com.marv.SpringEcom.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +12,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
 
     @GetMapping("/products")
     public List<Product> getProducts() {
-        return "All Products";
+        return productService.getAllProducts();
     }
 }
---
+++ src/main/java/com/marv/SpringEcom/service/ProductService.java
@@ -0,0 +1,19 @@
+package com.marv.SpringEcom.service;
+
+import com.marv.SpringEcom.model.Product;
+import com.marv.SpringEcom.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepo productRepo;
+
+    public List<Product> getAllProducts() {
+        return productRepo.findAll();
+    }
+}
