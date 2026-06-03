ac81a83419adf7427c8d6a5f46b976952aa3b905
MarvsConcept <Marvade8@gmail.com>
2026-06-03 14:51:39 -0700

    Created Order Controller and Started Order Service

---
+++ src/main/java/com/marv/SpringEcom/Controller/OrderController.java
@@ -0,0 +1,34 @@
+package com.marv.SpringEcom.Controller;
+
+import com.marv.SpringEcom.model.dto.OrderRequest;
+import com.marv.SpringEcom.model.dto.OrderResponse;
+import com.marv.SpringEcom.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping("/orders/place")
+    public ResponseEntity<OrderResponse> placeOrder(@RequestBody OrderRequest orderRequest) {
+
+        OrderResponse orderResponse = orderService.placeOrder(orderRequest);
+
+        return new ResponseEntity<>(orderResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/orders")
+    public ResponseEntity<List<OrderResponse>> getAllOrder() {
+        List<OrderResponse> responses = orderService.getAllOrderResponses();
+        return new ResponseEntity<>(responses, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/marv/SpringEcom/service/OrderService.java
@@ -0,0 +1,21 @@
+package com.marv.SpringEcom.service;
+
+import com.marv.SpringEcom.model.dto.OrderRequest;
+import com.marv.SpringEcom.model.dto.OrderResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+
+    public OrderResponse placeOrder(OrderRequest orderRequest) {
+
+        return null;
+    }
+
+    public List<OrderResponse> getAllOrderResponses() {
+
+        return null;
+    }
+}
